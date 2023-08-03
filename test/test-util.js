2cd7e69af61e5de3f2ad1f98122058a6e0b97102
mukhtasar93 <mukhtasar03@gmail.com>
2023-08-03 09:17:32 +0700

    search contact API and Finished Contact API

--- test/test-util.js
+++ test/test-util.js
@@ -47,6 +47,20 @@ export const createTestContact = async () => {
 	});
 };
 
+export const createManyTestContacts = async () => {
+	for (let i = 0; i < 15; i++) {
+		await prismaClient.contact.create({
+			data: {
+				username: "user",
+				first_name: `test ${i}`,
+				last_name: `test ${i}`,
+				email: `test${i}@gmail.com`,
+				phone: `0811223344${i}`,
+			},
+		});
+	}
+};
+
 export const getTestContact = async () => {
 	return prismaClient.contact.findFirst({
 		where: {
