bf96f5db4a60c228cad6771c740b701058e1eca1
mukhtasar93 <mukhtasar03@gmail.com>
2023-08-07 10:52:57 +0700

    feature remove address API

--- src/service/address-service.js
+++ src/service/address-service.js
@@ -110,8 +110,31 @@ const update = async (user, contactId, request) => {
 	});
 };
 
+const remove = async (user, contactId, addressId) => {
+	contactId = await checkContactMustExist(user, contactId);
+	addressId = validate(updateAddressValidation, addressId);
+
+	const totalAddressInDatabase = await prismaClient.address.count({
+		where: {
+			contact_id: contactId,
+			id: addressId,
+		},
+	});
+
+	if (!totalAddressInDatabase) {
+		throw new ResponseError(404, "Address is not found");
+	}
+
+	return prismaClient.address.delete({
+		where: {
+			id: addressId,
+		},
+	});
+};
+
 export default {
 	create,
 	get,
 	update,
+	remove,
 };
